8f8854cf6fb88f88c22a7c31a303c1122c626446
rklogan <ryan.k.logan@gmail.com>
2019-12-21 13:20:44 -0500

    Code Cleanup

--- Silly_Sampler.py
+++ Silly_Sampler.py
@@ -1,11 +1,12 @@
 import os
 from os.path import isfile, join
-#import scipy.io.wavfile as waves    #requires install; does not handle 24bit audio. 8, 16, 32 only.
 import numpy as np                  #requires install
 import mido as md                   #requires install
 import random
 import librosa                      #requires install
 import soundfile as sf              #requires install 
+
+VERBOSE = True
 
 def get_MIDI_note(sample_data, fs):
     pitches, magnitudes = librosa.core.piptrack(y=sample_data, sr=fs)
@@ -55,21 +56,21 @@ if __name__ == "__main__":
     all_samples_names = [f for f in os.listdir(samples_path) if isfile(join(samples_path, f))]
     used_samples = []
 
+    #process each track in the MIDI file
     for track in score_file.tracks:
         print('Processing ' + track.name + '...')
 
         output_buffer = np.zeros((int(score_file.length * fs),))
-        buffer_modified = False
-        tick = 0
+        buffer_modified = False         #avoids writing empty audio files
+        tick = 0                        #tick in MIDI clock
 
-        sample_dict = {}
-
+        sample_dict = {}                #will store pitched samples
         num_events = len(track)
-        print(num_events)
+        
         i = 0
         while i < num_events:
-            if i % 32 == 0:
-                print(str(100*i / num_events)+'%')
+            if VERBOSE and i % 32 == 0 and i != 0:
+                print(str(i) + ' of ' + str(num_events) + ' processed for track: ' + track.name + '\t\t\t\t' + str(int(100*i/num_events)) + '%')
             tick += track[i].time
 
             if track[i].type == 'note_on':
@@ -79,24 +80,15 @@ if __name__ == "__main__":
                         all_samples_names = used_samples.copy()
                         used_samples = []
 
-                    #sample_dict[track[i].note] = random.choice(all_samples_names)
-                    #all_samples_names.remove(sample_dict[track[i].note])
-                    #used_samples.append(sample_dict[track[i].note])
                     chosen_sample_name = random.choice(all_samples_names)
                     all_samples_names.remove(chosen_sample_name)
                     used_samples.append(chosen_sample_name)
-                    print(chosen_sample_name)
 
                     #load the sample
                     sample_data, _ = librosa.load(join(cwd_path, sample_directory, chosen_sample_name))
                     MIDI_note = get_MIDI_note(sample_data, fs)
                     sample_dict[track[i].note] = librosa.effects.pitch_shift(sample_data, fs, track[i].note - MIDI_note)
 
-                #load the sample
-                #sample_name = sample_dict[track[i].note]
-                #_, sample_data = waves.read(join(cwd_path, sample_directory, sample_name))
-                #sample_data, _ = librosa.load(join(cwd_path, sample_directory, sample_name))
-                
                 #write the sample to buffer
                 start_sample = int(md.tick2second(tick, score_file.ticks_per_beat, tempo*fs))
                 j = 0
@@ -105,47 +97,13 @@ if __name__ == "__main__":
                         break
                     
                     val = gain * track[i].velocity/127 * sample_dict[track[i].note][j]
-                    """try:
-                        val = gain * track[i].velocity/127 * sample_dict[track[i].note][j]
-                    except:
-                        print(str(i) + '\t' + str(j) + '\t')
-                        print(len(sample_dict[track[i].note]))
-                        print(range(len(sample_dict[track[i].note])))
-                        print(sample_dict[track[i].note])
-                        raise Exception"""
-                    
-                    """if track.name == 'Main':
-                        print('v' + str(track[i].velocity))
-                        print('sd' + str(sample_data[j]))
-                        print(j)
-                        print(val)"""
                     
                     if val != 0:
                         buffer_modified = True
                     output_buffer[start_sample+j] += val
             i += 1         
         
+        #write buffer to file
         if buffer_modified:
             print('Writing ' + track.name + ' to file')
-            #waves.write(join(cwd_path, 'output', track.name + '.wav'), fs, output_buffer)
             sf.write(join(cwd_path, 'Output', track.name + '.wav'), output_buffer, fs)
-
-    """
-    # wav tutorial
-    all_files = [f for f in os.listdir(samples_path) if isfile(join(samples_path, f))]
-
-    for f in all_files:
-        fs, data = waves.read(join(cwd_path, sample_directory, f))
-        new_data = np.zeros((2, len(data)), dtype=type(data[0]))
-
-        for i in range(len(data)):
-            new_data[0][i] = data[i]
-            new_data[1][i] = data[i]
-
-        if '440' in f:
-            waves.write(join(cwd_path, sample_directory, 'test.wav'), 44100, new_data)
-
-    op = waves.read(join(cwd_path, sample_directory, 'test.wav'))
-    print(op)"""
-
-
